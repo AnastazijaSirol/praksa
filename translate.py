b410246a0c40062b6475f477dcc6dbf165356402
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2025-02-19 11:53:33 +0100

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,9 +1,16 @@
 from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
 import sys
 import xml.etree.ElementTree as ET
+import logging
 
 model = M2M100ForConditionalGeneration.from_pretrained("facebook/m2m100_1.2B")
 tokenizer = M2M100Tokenizer.from_pretrained("facebook/m2m100_1.2B")
+
+logging.basicConfig(
+    filename="translation.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 '''def translate_text(input_text, src_lang, target_lang):
     tokenizer.src_lang = src_lang
@@ -13,15 +20,25 @@ tokenizer = M2M100Tokenizer.from_pretrained("facebook/m2m100_1.2B")
     return translated[0]'''
 
 def translate_text(input_text, src_lang, target_lang):
-    context = "In the context of the hospital care and medical examination: "
-    input_text_with_context = context + input_text
-    tokenizer.src_lang = src_lang
-    encoded = tokenizer(input_text_with_context, return_tensors="pt")
-    generated_tokens = model.generate(**encoded, forced_bos_token_id=tokenizer.get_lang_id(target_lang))
-    translated = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
-    translated_text = translated[0].replace(context, "")
-    print(translated_text)
-    return translated_text
+    try:
+        context = "In the context of the hospital care and medical examination: "
+        input_text_with_context = context + input_text
+        logging.info(f"Text: {input_text}")
+
+        tokenizer.src_lang = src_lang
+        encoded = tokenizer(input_text_with_context, return_tensors="pt")
+
+        generated_tokens = model.generate(**encoded, forced_bos_token_id=tokenizer.get_lang_id(target_lang))
+
+        translated = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+        translated_text = translated[0].replace(context, "")
+        logging.info(f"Translated text: {translated_text}")
+
+        return translated_text
+
+    except Exception as e:
+        logging.error(f"Error during translation: {e}", exc_info=True)
+        return None
 
 def translate_resx(file_path, src_lang, target_lang):
     tree = ET.parse(file_path)
@@ -42,3 +59,9 @@ if __name__ == "__main__":
 
     translated_xml = translate_resx(file_path, src_lang, target_lang)
     print(translated_xml)
+
+
+
+
+# koristiti manji model (418M) za ubrzati proces
+# finetuning modela za medicinske izraze 
